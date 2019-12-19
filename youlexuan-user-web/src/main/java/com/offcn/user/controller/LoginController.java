dfcd47ac118083274a3125726728ca637003510c
showhand88 <gualao233@163.com>
2019-12-19 14:34:04 +0800

    commit

---
+++ youlexuan-user-web/src/main/java/com/offcn/user/controller/LoginController.java
@@ -0,0 +1,25 @@
+package com.offcn.user.controller;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by travelround on 2019/7/9.
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+    @RequestMapping("/name")
+    public Map showName() {
+        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Map map = new HashMap<>();
+        map.put("loginName", name);
+        return map;
+    }
+
+}
